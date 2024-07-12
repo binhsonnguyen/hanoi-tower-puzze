dbf41d681bdc7981139e48bc9ab7a530383932c4
son <son@codegym.vn>
2024-07-12 14:59:02 +0700

    draw a rectangle

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,14 @@
-import { Application, Assets, Sprite } from 'pixi.js'
+import { Application, Graphics } from 'pixi.js'
 
 import './style.css'
-
-// The application will create a renderer using WebGL, if possible,
-// with a fallback to a canvas render. It will also setup the ticker
-// and the root stage PIXI.Container
 const app = new Application();
+const graphics = new Graphics();
 
-// Wait for the Renderer to be available
-await app.init();
-
-// The application will create a canvas element for you that you
-// can then insert into the DOM
+await app.init({ background: '#1099bb', resizeTo: window });
 document.body.appendChild(app.canvas);
 
-// load the texture we need
-const texture = await Assets.load('assets/bunny.png');
+// Rectangle
+graphics.rect(50, 50, 100, 100);
+graphics.fill(0xde3249);
 
-// This creates a texture from a 'bunny.png' image
-const bunny = new Sprite(texture);
-
-// Setup the position of the bunny
-bunny.x = app.renderer.width / 2;
-bunny.y = app.renderer.height / 2;
-
-// Rotate around the center
-bunny.anchor.x = 0.5;
-bunny.anchor.y = 0.5;
-
-// Add the bunny to the scene we are building
-app.stage.addChild(bunny);
-
-// Listen for frame updates
-app.ticker.add(() => {
-  // each frame we spin the bunny around a bit
-  bunny.rotation += 0.01;
-});
+app.stage.addChild(graphics);
