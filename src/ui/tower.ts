96324abb85e4da0ee6342f4d476af9d7370ec17d
son <son@codegym.vn>
2024-07-13 01:03:29 +0700

    draw tower's component base on parent

--- src/ui/tower.ts
+++ src/ui/tower.ts
@@ -1,8 +1,8 @@
 import { Container, Graphics } from 'pixi.js'
 
 const defaultTowerOptions = {
-  x: 200,
-  y: 200,
+  x: 50,
+  y: 120,
   height: 120,
   width: 100,
   color: 0x000000
@@ -21,12 +21,15 @@ export class Tower extends Container {
 
     this._options = { ...defaultTowerOptions, ...options }
 
+    this.x = this._options.x
+    this.y = this._options.y
+
     this._base = new Graphics()
-    this._base.rect(this._options.x - this._options.width / 2, this._options.y, this._options.width, 1)
+    this._base.rect(-this._options.width / 2, 0, this._options.width, 1)
     this._base.fill(this._options.color)
 
     this._pillar = new Graphics()
-    this._base.rect(this._options.x, this._options.y - this._options.height, 1, this._options.height)
+    this._base.rect(0, -this._options.height, 1, this._options.height)
     this._base.fill(this._options.color)
 
     this.addChild(this._base)
