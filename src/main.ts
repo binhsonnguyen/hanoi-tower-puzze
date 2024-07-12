c4f09ad5fa98ecbfbfaf05dfb858f5dc04a1c27f
son <son@codegym.vn>
2024-07-12 22:58:14 +0700

    reformat code

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { getUrlParam } from './util/urlParams'
 import { navigation } from './util/navigation'
 import { PuzzleScreen } from './screens/puzzleScreen'
 
-export const app = new Application();
+export const app = new Application()
 
 // Init everything
 init().then(() => {
@@ -15,32 +15,32 @@ init().then(() => {
 // End of execution
 
 /** Setup app and initialise assets */
-async function init() {
+async function init () {
   // Initialize app
   await app.init({
     resolution: Math.max(window.devicePixelRatio, 2),
     backgroundColor: 0x1099bb,
-  });
+  })
 
   // Add pixi canvas element (app.canvas) to the document's body
-  document.body.appendChild(app.canvas);
+  document.body.appendChild(app.canvas)
 
   // Whenever the window resizes, call the 'resize' function
-  window.addEventListener('resize', resize);
+  window.addEventListener('resize', resize)
 
   // Trigger the first resize
-  resize();
+  resize()
 
   // Add a visibility listener, so the app can pause sounds and screens
-  document.addEventListener('visibilitychange', visibilityChange);
+  document.addEventListener('visibilitychange', visibilityChange)
 
   // Go to one of the screens if a shortcut is present in url params, otherwise go to home-screen
-  if (getUrlParam('puzzle') !== null) {
-    await navigation.showScreen(PuzzleScreen);
-  } else if (getUrlParam('result') !== null) {
-    await navigation.showScreen(PuzzleScreen);
+  if (!!getUrlParam('puzzle')) {
+    await navigation.showScreen(PuzzleScreen)
+  } else if (!!getUrlParam('result')) {
+    await navigation.showScreen(PuzzleScreen)
   } else {
-    await navigation.showScreen(PuzzleScreen);
+    await navigation.showScreen(PuzzleScreen)
   }
 }
 
